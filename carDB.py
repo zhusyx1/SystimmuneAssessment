dee9e868ca6a0fe7d9ea6d9692a916acd27a9a04
zhusyx1 <74025814+zhusyx1@users.noreply.github.com>
2020-11-05 20:21:42 -0600

    Adding code for importing csv data into database

---
+++ carDB.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import pyodbc
+
+# Import CSV
+data = pd.read_csv (r'car.csv')   
+df = pd.DataFrame(data, columns= ['CarModleId','CarModleName','SalesName','SoldMonth','NumberSold'])
+
+# Connect to SQL Server
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=RON\SQLEXPRESS;'
+                      'Database=TestDB;'
+                      'Trusted_Connection=yes;')
+cursor = conn.cursor()
+
+# Create Table
+cursor.execute('CREATE TABLE car (
+    CarModleId int,
+    CarModleName varchar(0),
+    SalesName varchar(50),
+    SoldMonth varchar(10)
+    NumberSold int)')
+
+# Insert DataFrame to Table
+for row in df.itertuples():
+    cursor.execute('''
+                INSERT INTO Car (CarModleId,CarModleName,SalesName,SoldMonth,NumberSold)
+                VALUES (?,?,?)
+                ''',
+                row.CarModleId, 
+                row.CarModleName,
+                row.SalesName,
+                row.SoldMonth,
+                row.NumberSold
+                )
+conn.commit()
